e7079c23759bc09cdeee66a68d2d5efa94d3e1e9
Nicholas Lauder <nick.lauder@outlook.com>
2018-05-11 22:22:47 +1200

    Made PyTeaser class based

--- pyteaser.py
+++ pyteaser.py
@@ -63,195 +63,220 @@ stopWords = set([
 ])
 ideal = 20.0
 
+class Summarizer():
+    def __init__(self, url=None, title=None, text=None):
+        self.url = url
+        self.title = title
+        self.text = text
 
-def SummarizeUrl(url):
-    summaries = []
-    try:
-        article = grab_link(url)
-    except IOError:
-        print('IOError')
-        return None
+        self.article = None
 
-    if not (article and article.cleaned_text and article.title):
-        return None
+        self.summaries = []
+        self.score = None
 
-    summaries = Summarize(str(article.title),
-                          str(article.cleaned_text))
-    return summaries
+    def summarize_article(self):
+        if self.url:
+            self.summarize_url()
+        elif self.title and self.text:
+            self.summarize_text()
+        else:
+            print("Summaries could not be created!")
+            return None
 
+        return self.get_raw_summary()
 
-def Summarize(title, text):
-    summaries = []
-    sentences = split_sentences(text)
-    keys = keywords(text)
-    titleWords = split_words(title)
+    def get_raw_summary(self):
+        return self.summaries
 
-    if len(sentences) <= 5:
-        return sentences
+    def get_formatted_summary(self):
+        return self.summaries.join("")
 
-    #score setences, and use the top 5 sentences
-    ranks = score(sentences, titleWords, keys).most_common(5)
-    for rank in ranks:
-        summaries.append(rank[0])
+    def summarize_url(self):
+        try:
+            self.grab_link()
+        except IOError:
+            print('IOError')
+            return None
 
-    return summaries
-
-
-def grab_link(inurl):
-    #extract article information using Python Goose
-    from goose3 import Goose
-    try:
-        article = Goose().extract(url=inurl)
-        return article
-    except ValueError:
-        print('Goose failed to extract article from url')
-        return None
-    return None
-
-
-def score(sentences, titleWords, keywords):
-    #score sentences based on different features
-
-    senSize = len(sentences)
-    ranks = Counter()
-    for i, s in enumerate(sentences):
-        sentence = split_words(s)
-        titleFeature = title_score(titleWords, sentence)
-        sentenceLength = length_score(sentence)
-        sentencePosition = sentence_position(i+1, senSize)
-        sbsFeature = sbs(sentence, keywords)
-        dbsFeature = dbs(sentence, keywords)
-        frequency = (sbsFeature + dbsFeature) / 2.0 * 10.0
-
-        #weighted average of scores from four categories
-        totalScore = (titleFeature*1.5 + frequency*2.0 +
-                      sentenceLength*1.0 + sentencePosition*1.0) / 4.0
-        ranks[s] = totalScore
-    return ranks
-
-
-def sbs(words, keywords):
-    score = 0.0
-    if len(words) == 0:
-        return 0
-    for word in words:
-        if word in keywords:
-            score += keywords[word]
-    return (1.0 / fabs(len(words)) * score)/10.0
-
-
-def dbs(words, keywords):
-    if (len(words) == 0):
-        return 0
-
-    summ = 0
-    first = []
-    second = []
-
-    for i, word in enumerate(words):
-        if word in keywords:
-            score = keywords[word]
-            if first == []:
-                first = [i, score]
-            else:
-                second = first
-                first = [i, score]
-                dif = first[0] - second[0]
-                summ += (first[1]*second[1]) / (dif ** 2)
-
-    # number of intersections
-    k = len(set(keywords.keys()).intersection(set(words))) + 1
-    return (1/(k*(k+1.0))*summ)
-
-
-def split_words(text):
-    #split a string into array of words
-    try:
-        text = regex_sub(r'[^\w ]', '', text, flags=REGEX_UNICODE)  # strip special chars
-        return [x.strip('.').lower() for x in text.split()]
-    except TypeError:
-        print("Error while splitting characters")
-        return None
-
-
-def keywords(text):
-    """get the top 10 keywords and their frequency scores
-    ignores blacklisted words in stopWords,
-    counts the number of occurrences of each word
-    """
-    text = split_words(text)
-    numWords = len(text)  # of words before removing blacklist words
-    freq = Counter(x for x in text if x not in stopWords)
-
-    minSize = min(10, len(freq))  # get first 10
-    keywords = {x: y for x, y in freq.most_common(minSize)}  # recreate a dict
-
-    for k in keywords:
-        articleScore = keywords[k]*1.0 / numWords
-        keywords[k] = articleScore * 1.5 + 1
-
-    return keywords
-
-
-def split_sentences(text):
-    '''
-    The regular expression matches all sentence ending punctuation and splits the string at those points.
-    At this point in the code, the list looks like this ["Hello, world", "!" ... ]. The punctuation and all quotation marks
-    are separated from the actual text. The first s_iter line turns each group of two items in the list into a tuple,
-    excluding the last item in the list (the last item in the list does not need to have this performed on it). Then,
-    the second s_iter line combines each tuple in the list into a single item and removes any whitespace at the beginning
-    of the line. Now, the s_iter list is formatted correctly but it is missing the last item of the sentences list. The
-    second to last line adds this item to the s_iter list and the last line returns the full list.
-    '''
-    
-    sentences = regex_split(r'(?<![A-ZА-ЯЁ])([.!?]"?)(?=\s+\"?[A-ZА-ЯЁ])', text, flags=REGEX_UNICODE)
-    s_iter = zip(*[iter(sentences[:-1])] * 2)
-    s_iter = [''.join(map(str,y)).lstrip() for y in s_iter]
-    s_iter.append(sentences[-1])
-    return s_iter
-
-
-def length_score(sentence):
-    return 1 - fabs(ideal - len(sentence)) / ideal
-
-
-def title_score(title, sentence):
-    title = [x for x in title if x not in stopWords]
-    count = 0.0
-    for word in sentence:
-        if (word not in stopWords and word in title):
-            count += 1.0
-            
-    if len(title) == 0:
-        return 0.0
+        if not (self.article and self.article.cleaned_text and self.article.title):
+            return None
         
-    return count/len(title)
+        self.title = self.article.title
+        self.text = self.article.cleaned_text
+
+        self.summaries = self.summarize_text()
+
+    def summarize_text(self):
+        parser = Input_Parser(self.title, self.text)
+
+        scores = Scorer(parser)
+        #score setences, and use the top 5 sentences
+        self.score = scores.score()
+        ranks = self.score.most_common(5)
+        for rank in ranks:
+            self.summaries.append(rank[0])    
+
+    def grab_link(self):
+        #extract article information using Python Goose
+        from goose3 import Goose
+        try:
+            self.article = Goose().extract(self.url)
+        except ValueError:
+            print('Goose failed to extract article from url')
+
+class Input_Parser():
+    def __init__(self, title, text):
+        self.title  = title
+        self.text   = text
+
+        self.words          = self.split_words(self.text)
+        self.sentences      = self.split_sentences(self.text)
+        self.keywords       = self.get_keywords(self.words)
+        self.title_words    = self.split_words(self.title)
+
+    def split_words(self, text):
+        #split a string into array of words
+        try:
+            text = regex_sub(r'[^\w ]', '', text, flags=REGEX_UNICODE)  # strip special chars
+            return [x.strip('.').lower() for x in text.split()]
+        except TypeError:
+            print("Error while splitting characters")
+            return None
+
+    def get_keywords(self, text):
+        """get the top 10 keywords and their frequency scores
+        ignores blacklisted words in stopWords,
+        counts the number of occurrences of each word
+        """
+        numWords = len(text)  # of words before removing blacklist words
+        freq = Counter(x for x in text if x not in stopWords)
+
+        minSize = min(10, len(freq))  # get first 10
+        keywords = {x: y for x, y in freq.most_common(minSize)}  # recreate a dict
+
+        for k in keywords:
+            articleScore = keywords[k]*1.0 / numWords
+            keywords[k] = articleScore * 1.5 + 1
+
+        return keywords
+
+    def split_sentences(self, text):
+        '''
+        The regular expression matches all sentence ending punctuation and splits the string at those points.
+        At this point in the code, the list looks like this ["Hello, world", "!" ... ]. The punctuation and all quotation marks
+        are separated from the actual text. The first s_iter line turns each group of two items in the list into a tuple,
+        excluding the last item in the list (the last item in the list does not need to have this performed on it). Then,
+        the second s_iter line combines each tuple in the list into a single item and removes any whitespace at the beginning
+        of the line. Now, the s_iter list is formatted correctly but it is missing the last item of the sentences list. The
+        second to last line adds this item to the s_iter list and the last line returns the full list.
+        '''
+        
+        sentences = regex_split(r'(?<![A-ZА-ЯЁ])([.!?]"?)(?=\s+\"?[A-ZА-ЯЁ])', text, flags=REGEX_UNICODE)
+        s_iter = zip(*[iter(sentences[:-1])] * 2)
+        s_iter = [''.join(map(str,y)).lstrip() for y in s_iter]
+        s_iter.append(sentences[-1])
+        return s_iter
+
+class Scorer():
+    def __init__(self, parser):
+        self.parser = parser
+
+    def score(self):
+        #score sentences based on different features
+
+        senSize = len(self.parser.sentences)
+        ranks = Counter()
+        for i, s in enumerate(self.parser.sentences):
+            sentence = self.parser.split_words(s)
+            titleFeature = self.title_score(sentence)
+            sentenceLength = self.length_score(sentence)
+            sentencePosition = self.sentence_position(i+1, senSize)
+            sbsFeature = self.sbs(sentence)
+            dbsFeature = self.dbs(sentence)
+            frequency = (sbsFeature + dbsFeature) / 2.0 * 10.0
+
+            #weighted average of scores from four categories
+            totalScore = (titleFeature*1.5 + frequency*2.0 +
+                        sentenceLength*1.0 + sentencePosition*1.0) / 4.0
+            ranks[s] = totalScore
+        return ranks
+
+    def sbs(self, sentence):
+        score = 0.0
+        if len(sentence) == 0:
+            return 0
+        for word in sentence:
+            if word in self.parser.keywords:
+                score += self.parser.keywords[word]
+        return (1.0 / fabs(len(sentence)) * score)/10.0
 
 
-def sentence_position(i, size):
-    """different sentence positions indicate different
-    probability of being an important sentence"""
+    def dbs(self, sentence):
+        if (len(sentence) == 0):
+            return 0
 
-    normalized = i*1.0 / size
-    if 0 < normalized <= 0.1:
-        return 0.17
-    elif 0.1 < normalized <= 0.2:
-        return 0.23
-    elif 0.2 < normalized <= 0.3:
-        return 0.14
-    elif 0.3 < normalized <= 0.4:
-        return 0.08
-    elif 0.4 < normalized <= 0.5:
-        return 0.05
-    elif 0.5 < normalized <= 0.6:
-        return 0.04
-    elif 0.6 < normalized <= 0.7:
-        return 0.06
-    elif 0.7 < normalized <= 0.8:
-        return 0.04
-    elif 0.8 < normalized <= 0.9:
-        return 0.04
-    elif 0.9 < normalized <= 1.0:
-        return 0.15
-    else:
-        return 0
+        summ = 0
+        first = []
+        second = []
+
+        for i, word in enumerate(sentence):
+            if word in self.parser.keywords:
+                score = self.parser.keywords[word]
+                if first == []:
+                    first = [i, score]
+                else:
+                    second = first
+                    first = [i, score]
+                    dif = first[0] - second[0]
+                    summ += (first[1]*second[1]) / (dif ** 2)
+
+        # number of intersections
+        k = len(set(self.parser.keywords.keys()).intersection(set(sentence))) + 1
+        return (1/(k*(k+1.0))*summ)
+
+    def length_score(self, sentence):
+        return 1 - fabs(ideal - len(sentence)) / ideal
+
+
+    def title_score(self, sentence):
+        title = [x for x in self.parser.title_words if x not in stopWords]
+        count = 0.0
+        for word in sentence:
+            if (word not in stopWords and word in title):
+                count += 1.0
+                
+        if len(title) == 0:
+            return 0.0
+            
+        return count/len(title)
+
+
+    def sentence_position(self, i, size):
+        """different sentence positions indicate different
+        probability of being an important sentence"""
+
+        normalized = i*1.0 / size
+        if normalized < 0:
+            return 0
+        elif normalized <= 0.1:
+            return 0.17
+        elif normalized <= 0.2:
+            return 0.23
+        elif normalized <= 0.3:
+            return 0.14
+        elif normalized <= 0.4:
+            return 0.08
+        elif normalized <= 0.5:
+            return 0.05
+        elif normalized <= 0.6:
+            return 0.04
+        elif normalized <= 0.7:
+            return 0.06
+        elif normalized <= 0.8:
+            return 0.04
+        elif normalized <= 0.9:
+            return 0.04
+        elif normalized <= 1.0:
+            return 0.15
+        else:
+            return 0
